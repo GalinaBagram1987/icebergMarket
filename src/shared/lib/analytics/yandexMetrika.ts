81b8df8db17627070c982ffb904943a74138d77e
GalinaBagram1987 <gaby_ev@mail.ru>
2026-07-25 20:39:45 +1000

    added data in load metrica

--- src/shared/lib/analytics/yandexMetrika.ts
+++ src/shared/lib/analytics/yandexMetrika.ts
@@ -1,10 +1,20 @@
 declare global {
   interface Window {
-    ym: (method: string, counterId: number, params?: object) => void;
+    ym?: (method: string, counterId: number, params?: object) => void;
   }
 }
 
 const loadYandexMetrika = (): void => {
+  // 1. Проверяем, что мы в браузере (не на сервере)
+  if (typeof window === 'undefined') {
+    return;
+  }
+
+  // 2. Проверяем, что метрика ЕЩЕ НЕ загружена
+  if (window.ym) {
+    return;
+  }
+
   console.log('Яндекс.Метрика успешно загружена!');
   // Тут будет ваш стандартный код инициализации счетчика Яндекса
 };
