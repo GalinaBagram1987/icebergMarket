dfd9fb0ab8ec8c6ef1206ccd2743f22c6f524851
GalinaBagram1987 <gaby_ev@mail.ru>
2026-08-24 17:57:53 +1000

    fix(api request): change type-import

--- src/shared/api/apiMethods/catalog/index.ts
+++ src/shared/api/apiMethods/catalog/index.ts
@@ -1,3 +1,3 @@
-export type { BackendCategoryListItem, BackendCategoryListResponse } from './type';
+export type { BackendCategoryListItem, BackendMainCategory, MainCatalogResponse, FirstLevCategoryResponse } from './type';
 
 export { catalogRequest } from './catalogRequests';
